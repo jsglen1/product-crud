8890d7c3c76bcc79072bcd1c8456dd6c4297896f
jsglen1 <wtfdxd@gmail.com>
2024-03-08 17:49:53 -0500

    clean project

--- src/app/api/something/route.ts
+++
@@ -1,6 +0,0 @@
-import prisma from "@/libs/db";
-
-export async function GET() {
-  const products = await prisma.product.findMany();
-  return new Response("data " + JSON.stringify(products), { status: 200 });
-}
